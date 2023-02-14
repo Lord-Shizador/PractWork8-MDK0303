6cee303f762ad14b1d81dd4b589ec3b7f17f77e7
Lord Shizador <119564476+Lord-Shizador@users.noreply.github.com>
2023-02-14 08:43:05 +0300

    Add files via upload

---
+++ PractWork6/PractWork6/MainForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace PractWork6
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chipherFileButton = new System.Windows.Forms.Button();
+            this.openFileDialogButton = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.generate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // chipherFileButton
+            // 
+            this.chipherFileButton.Location = new System.Drawing.Point(314, 126);
+            this.chipherFileButton.Name = "chipherFileButton";
+            this.chipherFileButton.Size = new System.Drawing.Size(93, 23);
+            this.chipherFileButton.TabIndex = 0;
+            this.chipherFileButton.Text = "Зашифровать";
+            this.chipherFileButton.UseVisualStyleBackColor = true;
+            this.chipherFileButton.Click += new System.EventHandler(this.chipherFileButton_Click);
+            // 
+            // openFileDialogButton
+            // 
+            this.openFileDialogButton.Location = new System.Drawing.Point(12, 126);
+            this.openFileDialogButton.Name = "openFileDialogButton";
+            this.openFileDialogButton.Size = new System.Drawing.Size(75, 23);
+            this.openFileDialogButton.TabIndex = 1;
+            this.openFileDialogButton.Text = "Выбрать";
+            this.openFileDialogButton.UseVisualStyleBackColor = true;
+            this.openFileDialogButton.Click += new System.EventHandler(this.openFileDialogButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.folderBrowserDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Путь файла";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Переменные";
+            // 
+            // generate
+            // 
+            this.generate.Location = new System.Drawing.Point(153, 126);
+            this.generate.Name = "generate";
+            this.generate.Size = new System.Drawing.Size(75, 23);
+            this.generate.TabIndex = 4;
+            this.generate.Text = "Генерация";
+            this.generate.UseVisualStyleBackColor = true;
+            this.generate.Click += new System.EventHandler(this.generate_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(444, 161);
+            this.Controls.Add(this.generate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.openFileDialogButton);
+            this.Controls.Add(this.chipherFileButton);
+            this.MaximumSize = new System.Drawing.Size(460, 200);
+            this.MinimumSize = new System.Drawing.Size(460, 200);
+            this.Name = "MainForm";
+            this.Text = "PractWork6";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button chipherFileButton;
+        private System.Windows.Forms.Button openFileDialogButton;
+        private System.Windows.Forms.OpenFileDialog folderBrowserDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button generate;
+    }
+}
+
---
+++ PractWork6/PractWork6/MainForm.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace PractWork6
+{
+    public partial class MainForm : Form
+    {
+        FolderBrowserDialog fbd = new FolderBrowserDialog();
+        int p = 0, q = 0, n, o, ecs, d;
+        List<int> decrypte = new List<int>();
+        List<int> symbol = new List<int>();
+        List<int> data2 = new List<int>();
+        List<int> decrypteList = new List<int>();
+        List<int> decrypteList2 = new List<int>();
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void chipherFileButton_Click(object sender, EventArgs e)
+        {
+            if (p != 0)
+            {
+                foreach (var item in Directory.GetFiles(fbd.SelectedPath))//
+                {
+                    decrypte.Clear();
+                    data2.Clear();
+                    decrypteList.Clear();
+                    decrypteList2.Clear();
+                    symbol.Clear();
+                    var bytes = File.ReadAllBytes(item); // ШУУУУУУ
+                    foreach (var files in bytes)
+                    {
+                        symbol.Add(Convert.ToInt32(files));
+                    }
+                    List<byte> decrypteSymbols = new List<byte>();
+                    List<byte> data = new List<byte>();
+                    Encrypte(symbol);
+                    foreach (var item1 in decrypteList)
+                    {
+                        decrypteSymbols.Add(Convert.ToByte(item1));
+                    }
+                    String[] s = item.Split(Path.DirectorySeparatorChar);//
+                    string trimmed = s[s.Length - 1];//                   
+                    File.WriteAllBytes("C:\\Для студентов\\PKS-91\\Encrypte\\" + trimmed, decrypteSymbols.ToArray());//
+                    var k = decrypte;
+                    bytes = File.ReadAllBytes("C:\\Для студентов\\PKS-91\\Encrypte\\" + trimmed);//
+                    decrypteList2 = new List<int>();
+                    string b = "";
+                    for (int i = 0; i < bytes.Count(); i += 2)
+                    {
+                        b = "";
+                        while (Convert.ToString(bytes[i + 1], 2).Length + b.Length < 8)
+                        {
+                            b += "0";
+                        }
+                        b = b + Convert.ToString(bytes[i + 1], 2);
+                        decrypteList2.Add(Convert.ToInt32(Convert.ToString(bytes[i], 2) + b, 2));
+                    }
+                    Decrypte();
+                    foreach (var item1 in data2)
+                    {
+                        data.Add(Convert.ToByte(item1));
+                    }
+                    label2.Text = "p = " + p + ";" + "q = " + q + ";" + "n = " + n + ";" + "o = " + o + ";" + "e = " + ecs + ";" + "d = " + d;
+                    File.WriteAllBytes("C:\\Для студентов\\PKS-91\\Decrypte\\"+trimmed,data.ToArray());
+                    //File.WriteAllBytes("Расшифрованный файл." + item.Split('.')[item.Split('.').Count() - 1], data.ToArray());
+
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Сгенерируйте числа");
+            }
+            MessageBox.Show("Готово, файл зашифрован и расшифрован");
+
+        }
+
+        private void generate_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            while (n < 255)
+            {
+                int[] a = Sieve(100);
+                p = a[rnd.Next(a.Length)];
+                q = a[rnd.Next(a.Length)];
+                n = q * p;
+            }
+            o = (p - 1) * (q - 1);
+            int nod = 1;
+            while (true)
+            {
+                ecs = rnd.Next(2, o);
+                for (int i = 1; i <= o; i++)
+                {
+                    if (ecs % i == 0 && o % i == 0)
+                    {
+                        nod = i;
+
+                    }
+
+                }
+                if (nod == 1)
+                {
+                    break;
+                }
+            }
+            d = 1;
+
+            while (true)
+            {
+                if ((d * ecs) % o == 1)
+                {
+                    break;
+                }
+                else
+                {
+                    d++;
+                }
+            }
+        }
+
+        static int[] Sieve(int n)
+        {
+            int[] S = Enumerable.Range(0, n + 1).ToArray();
+
+            S[1] = 0;
+
+            for (int k = 2; k * k <= n; k++)
+            {
+                if (S[k] != 0)
+                {
+                    for (int l = k * k; l <= n; l += k)
+                    {
+                        S[l] = 0;
+                    }
+                }
+            }
+            return S.Where(x => x != 0).ToArray();
+        }
+
+        private void openFileDialogButton_Click(object sender, EventArgs e)
+        {
+            decrypte.Clear();
+            data2.Clear();
+            decrypteList.Clear();
+            decrypteList2.Clear();
+            symbol.Clear();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                label1.Text = fbd.SelectedPath;
+
+                /* var bytes = File.ReadAllBytes(folderBrowserDialog1.FileName);
+                 foreach (var item in bytes)
+                 {
+                     symbol.Add(Convert.ToInt32(item));
+                 }*/
+            }
+        }
+        private void Encrypte(List<int> symbols)
+        {
+            decrypteList = new List<int>();
+            int c, n2;
+
+            c = 1;
+            n2 = 0;
+            string num1, num2;
+            int num3, num4;
+            string a1 = "";
+            decrypteList = new List<int>();
+            string binaryString;
+            foreach (var itm in symbols)
+            {
+                a1 = "";
+                do
+                {
+                    n2++;
+                    c = (itm * c) % n;
+                }
+                while (n2 < ecs);
+                binaryString = Convert.ToString(c, 2);
+                string h = "";
+                while (binaryString.Length + h.Length < 16)
+                {
+                    h += "0";
+                }
+                binaryString = h + binaryString;
+                decrypte.Add(Convert.ToInt32(binaryString, 2));
+                for (int i = 0; i < 8; i++)
+                {
+                    a1 += binaryString[i];
+                }
+                num1 = Convert.ToString(a1);
+                a1 = "";
+                for (int i = 8; i < 16; i++)
+                {
+                    a1 += binaryString[i];
+                }
+                num2 = Convert.ToString(a1);
+                a1 = "";
+                num3 = Convert.ToInt32(num1, 2);
+                num4 = Convert.ToInt32(num2, 2);
+                decrypteList.Add(num3);
+                decrypteList.Add(num4);
+                n2 = 0;
+                c = 1;
+            }
+        }
+        private void Decrypte()
+        {
+            int c = 1, n1 = 0;
+
+            foreach (var itm in decrypteList2)
+            {
+                do
+                {
+                    n1++;
+                    c = (Convert.ToInt32(itm) * c) % n;
+                }
+                while (n1 < d);
+                data2.Add(c);
+                n1 = 0;
+                c = 1;
+            }
+        }
+    }
+}
